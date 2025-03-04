7052f583b3ca60b0ace2bec83b78397b79c15655
Joe Lin <jlin249@my.bcit.ca>
2025-03-04 03:11:33 -0800

    modified base url

--- BlazorSchool/Models/Constants.cs
+++ BlazorSchool/Models/Constants.cs
@@ -4,6 +4,6 @@ namespace BlazorSchool.Models;
 
 public class Constants
 {
-    public const string BASE_URL = "/";
+    public const string BASE_URL = "https://icy-bush-08c8bfc10.6.azurestaticapps.net/";
 }
 
